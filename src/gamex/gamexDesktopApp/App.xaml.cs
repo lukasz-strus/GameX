52f5a792b9cdbee9b41a56cb826e4f13bccde04b
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2022-08-11 20:54:28 +0200

    Adding EntityFramework library

--- src/gamex/gamexDesktopApp/App.xaml.cs
+++ src/gamex/gamexDesktopApp/App.xaml.cs
@@ -45,7 +45,7 @@ public partial class App : Application
     }
 
     //TODO CLEAN CODE
-    //TODO CC1. HttpStatusCode
+    //TODO CC1 Entity Framework
     //TODO CC2. Rozdzielić Metody
     //TODO CC3. Wydzielić metody pomocnicze dla komend
     //TODO CC4. Generyczność komend
---
+++ src/gamex/gamexEntities/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace gamexEntities;
+
+public class Image
+{
+    public int Id { get; set; }
+    public byte[] ImageStream { get; set; }
+    public int GameId { get; set; }
+    public virtual Game Game { get; set; }
+    public string Extension { get; set; }
+}
